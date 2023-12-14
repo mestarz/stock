3ffc8da324cf80fd369dbd409a25a67da9744583
xuanlv <starysky.yaoding@icloud.com>
2023-12-14 23:49:49 +0800

    使用正态分布划分区间

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ import random
 import numpy
 import numpy as np
 import datetime
-
+from scipy.stats import norm
 import utils
 
 
@@ -48,10 +48,16 @@ class DataMacine:
     def sample_only_price(self, symbol: str, frequency: utils.Frequency, year: int) -> [np.array]:
         data = self.sample(symbol, frequency, year)
         close = [data[i][0] for i in range(len(data))]
-        # 收盘价标准化
-        close = self.normalize_close(close)
         result = [[close[i]] for i in range(len(close))]
         return np.array(result)
+
+    def sample_only_price_volatility(self, symbol: str, frequency: utils.Frequency, year: int) -> [np.array]:
+        data = self.sample_only_price(symbol, frequency, year)
+        volatility = [(data[i] - data[i - 1]) / data[i - 1] for i in range(1, len(data))]
+        volatility = np.array(volatility).flatten()
+        volatility = [0] + volatility
+        volatility = [volatility[i] * 100 for i in range(len(volatility))]
+        return volatility
 
     def sample_normalize_n(self, symbol: str, frequency: utils.Frequency, year: int, n: int) -> [np.array]:
         sample_num_back = self.sample_num
@@ -85,3 +91,50 @@ class DataMacine:
         # 取六位小数
         normalize1 = [round(normalize1[i], 6) for i in range(len(normalize1))]
         return normalize1
+
+
+class Normalize:
+    def __init__(self, field: int, data: [float]):
+        # 保留元数据
+        self.src_data = data
+        # 计算绝对值
+        self.abs_data = [abs(data[i]) for i in range(len(data))]
+        # 计算标准差
+        self.std = np.std(self.abs_data)
+        # 计算平均值
+        self.mean = np.mean(self.abs_data)
+
+        num_step = 100 * field
+        # 创建一组 x 值
+        x = np.linspace(0, 10, num_step)
+        # 计算正态分布的概率密度函数
+        pdf_values = norm.pdf(x, self.mean, self.std)
+        # 计算累计概率密度函数
+        step = (10 - 0) / num_step
+        li = [0]
+        for i in range(1, len(pdf_values)):
+            li.append(li[i - 1] + step * pdf_values[i - 1])
+        li.append(1)
+
+        # 划分区间
+        field_step = 1 / (field / 2)
+        self.slice = []
+        start = 0
+        for i in range(len(li)):
+            if li[i] >= start:
+                self.slice.append(i / num_step)
+                start += field_step
+
+        self.slice = [-x for x in reversed(self.slice)][:-1] + self.slice
+        self.slice = np.array(self.slice) * 10
+
+    def normalize(self, data: float) -> int:
+        for i in range(len(self.slice)):
+            if i == len(self.slice) - 1:
+                return i
+            if self.slice[i] <= data < self.slice[i + 1]:
+                return i
+        return len(self.slice) - 1
+
+    def normalize_list(self, data: [float]) -> [int]:
+        return [self.normalize(data[i]) for i in range(len(data))]
