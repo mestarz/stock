0361ff469794c1114d1f60ef82bcf7518e4340bf
xuanlv <starysky.yaoding@icloud.com>
2023-12-21 00:05:16 +0800

    绘制AO指标

--- src/database/database.py
+++ src/database/database.py
@@ -1,3 +1,4 @@
+import random
 from datetime import datetime
 from gm.api import *
 from pandas import DataFrame
@@ -5,7 +6,8 @@ from pandas import DataFrame
 import pandas as pd
 import src.configure.configure as configure
 from enum import Enum
-import mplfinance as mpl
+import mplfinance as mpf
+from finta import TA
 
 # 设置token， 查看已有token ID,在用户-密钥管理里获取
 set_token(configure.token)
@@ -27,9 +29,56 @@ def get_n_data_before_now(symbol: str, frequency: Frequency, count: int) -> Data
 
 
 # 获取开盘价、收盘价、最高价、最低价
-def get_n_price_before_now(symbol: str, frequency: Frequency, count: int):
+def get_n_price_before_now(symbol: str, frequency: Frequency, count: int) -> DataFrame:
     data = get_n_data_before_now(symbol, frequency, count)
     return data[['open', 'close', 'high', 'low']]
+
+
+# 计算AO指标
+# 最近5根平均移动值 - 最近34根平均移动值
+def set_ao(data: DataFrame) -> DataFrame:
+    # 计算AO指标
+    data['ao'] = TA.AO(data)
+    return data
+
+
+def color_func(x):
+    if x > 0:
+        return 'g'
+    else:
+        return 'r'
+
+
+# 绘制AO柱状图
+def draw_kline_with_ao(data: DataFrame):
+    dates = pd.date_range(start='01-01-2020', periods=len(data))
+    data.set_index(dates, inplace=True)
+    # 创建一个figure
+    fig = mpf.figure(style='yahoo', figsize=(7, 8))
+
+    # 添加一个subplot用于绘制K线图
+    ax1 = fig.add_subplot(2, 1, 1)
+    mpf.plot(data, ax=ax1, type='candle')
+
+    def color_list_func(input_datas: DataFrame) -> [str]:
+        ao_list = input_datas['ao']
+        colors = ['r']
+        for i in range(1, len(ao_list)):
+            if ao_list[i] > ao_list[i - 1]:
+                colors.append('r')
+            elif ao_list[i] == ao_list[i - 1]:
+                colors.append(colors[i - 1])
+            else:
+                colors.append('g')
+        return colors
+
+    color_list = color_list_func(data)
+    # 添加一个subplot用于绘制AO指标图
+    ax2 = fig.add_subplot(2, 1, 2)
+    ax2.bar(data.index, data['ao'], alpha=0.7, color=color_list)
+
+    # 显示图表
+    mpf.show()
 
 
 # 绘制k线图
@@ -37,31 +86,31 @@ def draw_k_line(data: DataFrame):
     # 绘制k线图
     dates = pd.date_range(start='01-01-2020', periods=len(data))
     data.set_index(dates, inplace=True)
-    mpl.plot(data, type='candle')
+    mpf.plot(data, type='candle')
 
 
 def draw_3k_line(day: DataFrame, hour: DataFrame, quarter: DataFrame):
-    fig = mpl.figure(style='yahoo', figsize=(7, 8))
+    fig = mpf.figure(style='yahoo', figsize=(7, 8))
 
     date_day = pd.date_range(start='01-01-2020', periods=len(day))
     day.set_index(date_day, inplace=True)
     ax1 = fig.add_subplot(3, 1, 1)
-    mpl.plot(day, type='candle', ax=ax1)
+    mpf.plot(day, type='candle', ax=ax1)
     ax1.set_title('day')
 
     date_hour = pd.date_range(start='01-01-2020', periods=len(hour))
     hour.set_index(date_hour, inplace=True)
     ax2 = fig.add_subplot(3, 1, 2, sharex=ax1)
-    mpl.plot(hour, type='candle', ax=ax2)
+    mpf.plot(hour, type='candle', ax=ax2)
     ax2.set_title('hour')
 
     date_quarter = pd.date_range(start='01-01-2020', periods=len(quarter))
     quarter.set_index(date_quarter, inplace=True)
     ax3 = fig.add_subplot(3, 1, 3, sharex=ax1)
-    mpl.plot(quarter, type='candle', ax=ax3)
+    mpf.plot(quarter, type='candle', ax=ax3)
     ax3.set_title('quarter')
 
-    mpl.show()
+    mpf.show()
 
 
 # 绘制鳄鱼线
@@ -71,9 +120,9 @@ def draw_alligator_line(day: DataFrame, hour: DataFrame, quarter: DataFrame):
     day.set_index(dates, inplace=True)
     hour.set_index(dates, inplace=True)
     quarter.set_index(dates, inplace=True)
-    mpl.plot(day, type='candle')
-    mpl.plot(hour, type='candle')
-    mpl.plot(quarter, type='candle')
+    mpf.plot(day, type='candle')
+    mpf.plot(hour, type='candle')
+    mpf.plot(quarter, type='candle')
 
 
 # 日k：14
@@ -84,4 +133,6 @@ if __name__ == '__main__':
     d1 = get_n_price_before_now('SHSE.510300', Frequency.Frequency_H, 100)
     d2 = get_n_price_before_now('SHSE.510300', Frequency.Frequency_M, 100)
     d3 = get_n_price_before_now('SHSE.510300', Frequency.Frequency_L, 100)
-    draw_3k_line(d1, d2, d3)
+    # draw_3k_line(d1, d2, d3)
+    d4 = set_ao(d1)
+    draw_kline_with_ao(d4)
